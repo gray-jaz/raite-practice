db53dfd71276f6b7ae3e70ecd0f148d6e602f935
erikasraymundo <erika.raymundo.s@bulsu.edu.ph>
2021-10-20 23:44:33 +0800

    Update quiz_maker.sql

--- quiz_maker.sql
+++ quiz_maker.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 20, 2021 at 04:32 AM
+-- Generation Time: Oct 20, 2021 at 09:10 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.10
 
@@ -30,20 +30,37 @@ SET time_zone = "+00:00";
 CREATE TABLE `question` (
   `ID` int(11) NOT NULL,
   `Title` varchar(250) NOT NULL,
-  `Type` int(11) NOT NULL,
+  `Type` varchar(5) NOT NULL,
   `Duration` int(11) NOT NULL,
-  `Active` int(11) NOT NULL,
+  `Active` int(11) NOT NULL DEFAULT 0,
   `Point` int(11) NOT NULL,
-  `Choice1` varchar(250) NOT NULL,
-  `Choice2` varchar(250) NOT NULL,
-  `Choice3` varchar(250) NOT NULL,
-  `Choice4` varchar(250) NOT NULL,
-  `SoloAnswer` varchar(250) NOT NULL,
-  `CheckAnswer` tinyint(1) NOT NULL,
-  `ChoiceAnswerKey` int(11) NOT NULL,
-  `OrderNo` int(11) NOT NULL,
+  `Choice1` varchar(250) DEFAULT NULL,
+  `Choice2` varchar(250) DEFAULT NULL,
+  `Choice3` varchar(250) DEFAULT NULL,
+  `Choice4` varchar(250) DEFAULT NULL,
+  `SoloAnswer` varchar(250) DEFAULT NULL,
+  `CheckAnswer` tinyint(1) DEFAULT NULL,
+  `ChoiceAnswerKey` char(1) DEFAULT NULL,
   `QuizID` varchar(8) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `question`
+--
+
+INSERT INTO `question` (`ID`, `Title`, `Type`, `Duration`, `Active`, `Point`, `Choice1`, `Choice2`, `Choice3`, `Choice4`, `SoloAnswer`, `CheckAnswer`, `ChoiceAnswerKey`, `QuizID`) VALUES
+(1, 'ff', 'TypeB', 30000, 0, 67, NULL, NULL, NULL, NULL, 'tre', NULL, NULL, '1E6HVi0P'),
+(2, 'erika', 'TypeA', 30000, 0, 25, 'aa', 'cc', 'bb', 'dd', NULL, NULL, 'd', '79cwHvdD'),
+(3, 'erika', 'TypeA', 30000, 0, 25, 'aa', 'cc', 'bb', 'dd', NULL, NULL, 'd', 'sZxIv9Hc'),
+(4, 'cue', 'TypeA', 60000, 0, 25, 'nn', 'uu', 'cc', 'mm', NULL, NULL, 'c', 'sZxIv9Hc'),
+(5, 'question', 'TypeB', 120000, 0, 40, NULL, NULL, NULL, NULL, '500', NULL, NULL, 'JdmEe2TG'),
+(6, 'agsagasgaga', 'TypeC', 120000, 0, 40, NULL, NULL, NULL, NULL, NULL, 0, NULL, '9crxheYT'),
+(7, 'bbb', 'TypeC', 120000, 0, 40, NULL, NULL, NULL, NULL, NULL, 0, NULL, '4gmGVw2F'),
+(8, 'agaga', 'TypeA', 30000, 0, 13131, 'aga', 'aavvv', 'agag', '131', NULL, NULL, 'd', '4gmGVw2F'),
+(9, 'bbb', 'TypeB', 120000, 0, 50, NULL, NULL, NULL, NULL, '50', NULL, NULL, '4gmGVw2F'),
+(10, '131', 'TypeA', 30000, 0, 3131, '131', '313131', '31', '31', NULL, NULL, 'b', 'HDa8Uznk'),
+(11, '31', 'TypeA', 30000, 0, 3131, '31', '1313', '3', '3131313131', NULL, NULL, 'd', 'QzJq8dBX'),
+(12, '31', 'TypeA', 30000, 0, 3131, '31', '1313', '3', '3131313131', NULL, NULL, 'd', 'XEMlNz3c');
 
 -- --------------------------------------------------------
 
@@ -53,9 +70,36 @@ CREATE TABLE `question` (
 
 CREATE TABLE `quiz` (
   `Code` varchar(8) NOT NULL,
-  `Status` int(11) NOT NULL,
-  `TeacherID` int(11) NOT NULL
+  `Status` int(11) NOT NULL DEFAULT 0,
+  `DateCreated` date NOT NULL DEFAULT current_timestamp(),
+  `TeacherUsername` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `quiz`
+--
+
+INSERT INTO `quiz` (`Code`, `Status`, `DateCreated`, `TeacherUsername`) VALUES
+('', 0, '2021-10-20', 'erikasong'),
+('1E6HVi0P', 0, '2021-10-20', 'erikasong'),
+('49VG1Prq', 0, '2021-10-20', 'erikasong'),
+('4gmGVw2F', 0, '2021-10-20', 'erikasong'),
+('79cwHvdD', 0, '2021-10-20', 'erikasong'),
+('9crxheYT', 0, '2021-10-20', 'erikasong'),
+('bgvA5oBt', 0, '2021-10-20', 'erikasong'),
+('fcIUFqnt', 0, '2021-10-20', 'erikasong'),
+('fK40j4J6', 0, '2021-10-20', 'erikasong'),
+('HDa8Uznk', 0, '2021-10-20', 'erikasong'),
+('HGMF6JHv', 0, '2021-10-20', 'erikaraymundo'),
+('JdmEe2TG', 0, '2021-10-20', 'erikasong'),
+('Jxuipf3K', 0, '2021-10-20', 'erikaraymundo'),
+('pQQcBMBP', 0, '2021-10-20', 'erikasong'),
+('QzJq8dBX', 0, '2021-10-20', 'erikasong'),
+('RG5Tmqii', 0, '2021-10-20', 'erikasong'),
+('sSVXZvSr', 0, '2021-10-20', 'erikasong'),
+('sZxIv9Hc', 0, '2021-10-20', 'erikasong'),
+('X7uNgYT4', 0, '2021-10-20', 'erikasong'),
+('XEMlNz3c', 0, '2021-10-20', 'erikasong');
 
 -- --------------------------------------------------------
 
@@ -75,8 +119,7 @@ CREATE TABLE `student` (
 --
 
 CREATE TABLE `teacher` (
-  `ID` int(11) NOT NULL,
-  `Username` varchar(50) NOT NULL,
+  `Username` varchar(25) NOT NULL,
   `Name` varchar(150) NOT NULL,
   `Password` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -85,11 +128,9 @@ CREATE TABLE `teacher` (
 -- Dumping data for table `teacher`
 --
 
-INSERT INTO `teacher` (`ID`, `Username`, `Name`, `Password`) VALUES
-(1, 'erikasong', 'Erika', '1234'),
-(2, 'erikasong19', 'Erika Simbulan Raymundo', ''),
-(4, '[object Object]', '[object Object]', '[object Object]'),
-(17, 'erikaraymundo', 'Raymundo, Ganda', '1234');
+INSERT INTO `teacher` (`Username`, `Name`, `Password`) VALUES
+('erikaraymundo', '13131', '131'),
+('erikasong', 'Erika Raymundo', '1234');
 
 --
 -- Indexes for dumped tables
@@ -107,7 +148,7 @@ ALTER TABLE `question`
 --
 ALTER TABLE `quiz`
   ADD PRIMARY KEY (`Code`),
-  ADD KEY `TeacherID` (`TeacherID`);
+  ADD KEY `TeacherID` (`TeacherUsername`);
 
 --
 -- Indexes for table `student`
@@ -119,8 +160,7 @@ ALTER TABLE `student`
 -- Indexes for table `teacher`
 --
 ALTER TABLE `teacher`
-  ADD PRIMARY KEY (`ID`),
-  ADD UNIQUE KEY `Username` (`Username`);
+  ADD PRIMARY KEY (`Username`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -130,19 +170,13 @@ ALTER TABLE `teacher`
 -- AUTO_INCREMENT for table `question`
 --
 ALTER TABLE `question`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `student`
 --
 ALTER TABLE `student`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `teacher`
---
-ALTER TABLE `teacher`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- Constraints for dumped tables
@@ -158,7 +192,7 @@ ALTER TABLE `question`
 -- Constraints for table `quiz`
 --
 ALTER TABLE `quiz`
-  ADD CONSTRAINT `quiz_ibfk_1` FOREIGN KEY (`TeacherID`) REFERENCES `teacher` (`ID`);
+  ADD CONSTRAINT `quiz_ibfk_1` FOREIGN KEY (`TeacherUsername`) REFERENCES `teacher` (`Username`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
